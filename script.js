0023e1e91e7c574a983f6c82afb4b1acd10a5c04
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-04-08 11:08:05 +0400

    made some changes

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let button = document.querySelector("button");
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
     let p = document.createElement("p");
     p.style.margin = "0 50px";
     p.style.color = "white";
     p.style.backgroundColor = "black";
     p.style.padding = "50px";
     fetch("https://api.chucknorris.io/jokes/random")
-    .then(res=>res.json())
-    .then(data=> p.innerText = data.value);
-   document.body.appendChild(p)
-    setTimeout(()=>{p.remove()},10000)
+        .then(res => res.json())
+        .then(data => p.innerText = data.value);
+    document.body.appendChild(p)
 });
